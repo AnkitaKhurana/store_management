b98f33fb8f475c18b721c52ebff5faf87b5a0d9e
Ankita Khurana <ankita0096@gmail.com>
2017-07-30 15:33:34 +0530

    server

---
+++ Store/routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router;
+
+const route = router();
+
+route.use('/events', require('./events'));
+route.use('/users', require('./users'));
+
+module.exports = route;
